ad389867fbf1977535bce4612b09921a781e6a43
[Olivia Winteringham] <owinteringham@gmail.com>
2019-04-02 00:21:58 +0100

    inital front end commit

---
+++ Documents/schoolOfCode/card-project-front-end/src/Components/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <>
+        <p> I'm making a new project in React </p>
+        <p>and I've just set up the basic template to start my database using Express </p>
+        </>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
