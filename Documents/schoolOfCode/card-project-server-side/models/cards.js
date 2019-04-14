d945fbc0dd4b7dd516acb10bd019b9e00957919a
[Olivia Winteringham] <owinteringham@gmail.com>
2019-04-14 19:19:39 +0100

    second save

--- Documents/schoolOfCode/card-project-server-side/models/cards.js
+++ Documents/schoolOfCode/card-project-server-side/models/cards.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose")
-const cardSchema = new mongoose.Schema({
+const cardSchema = new mongoose.Schema
+({
 
     suit: {type: String, required: true, unique: true},
     value: String,
@@ -13,20 +13,4 @@ const cardSchema = new mongoose.Schema({
 
 });
 
-=======
-'use strict';
-
-var mongoose = require("mongoose");
-
-var cardSchema = new mongoose.Schema({
-    suit: { type: String, required: true },
-    value: Number,
-    description: { type: String, required: true },
-    locationFound: { type: String, required: true },
-    date: Date,
-    whoFoundIt: String,
-    filth: Number,
-    imageURL: String,
-});
->>>>>>> 818396aa13f324f260b27465e1554f58e3a74c9f
 module.exports = mongoose.model("card", cardSchema);
