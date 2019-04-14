192a53c19a274a64594c83b2f86c73809be0450c
[Olivia Winteringham] <owinteringham@gmail.com>
2019-04-14 19:17:24 +0100

    attempts at GET request, data.json added

---
+++ Documents/schoolOfCode/card-project-server-side/models/cards.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const cardSchema = new mongoose.Schema({
+
+    suit: {type: String, required: true, unique: true},
+    value: String,
+    description: String,
+    locationDiscovered: {type: String, required: true},
+    dateDiscovered: Date,
+    whoFoundIt: String,
+    filth: String,
+    imageUrl: String
+
+});
+
+module.exports = mongoose.model("card", cardSchema);
--- Documents/schoolOfCode/card-project-server-side/models/users.js
+++ Documents/schoolOfCode/card-project-server-side/models/users.js
@@ -1,24 +1,23 @@
-'use strict';
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
-var mongoose = require("mongoose");
-var bcrypt = require("bcryptjs");
 
-var userSchema = new mongoose.Schema({
-  firstName: String,
+var userSchema = new.mongoose.Schema({
+  firstName: {type: String, required: true},
   lastName: String,
-  email: { type: String, unique: true },
-  password: String
+  email: {type: String, required: true},
+  password: {type: String, required: true}
 });
 
-userSchema.pre("save", function(next) {
+userSchema.pre("save", function(next){
   const user = this;
-  if (!user.isModified("password")) {
-    return next();
-  }
-  bcrypt.hash(user.password, 10).then(hashedPassword => {
-    user.password = hashedPassword;
-    next();
-  });
+if (!user.isModified("password")){
+  return next();
+}
+bcrypt.hash(user.password, 10).then(hashedPassword => {
+  user.password = hashedPassword;
+  next();
+});
 });
 
 module.exports = mongoose.model("user", userSchema);
